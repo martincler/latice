67c64b560795e253a0071cdcef80a6e7ee7e60c9
martin <martin.clerot@etu.unilim.fr>
2025-03-31 18:38:03 +0200

    ajout LaticeApplicationConsole

---
+++ src/main/java/latrice/application/LaticeApplicationConsole.java
@@ -0,0 +1,12 @@
+package latrice.application;
+
+public class LaticeApplicationConsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----------------------------------------------------");
+		System.out.println("-- Bienvenue dans notre magnifique jeu de latice ! --");
+		System.out.println("-----------------------------------------------------");
+	}
+
+}
